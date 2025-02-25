c5d961cff8b9127c0d1eee0812097e9a319079f6
Even Luiv <luiveven7@gmail.com>
2025-02-25 11:44:01 +0200

    edit linting config

---
+++ eslint.config.cjs
@@ -0,0 +1,17 @@
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+
+module.exports = [
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+  },
+]
